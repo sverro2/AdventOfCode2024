cfdf649e1f5a7d65a6945da4869c2f315c53b031
Sven Brettschneider <svenbrettschneider@proton.me>
2024-12-01 21:49:59 +0100

    even more needless optimalisations

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -14,16 +14,16 @@ fn main() {
     let parsed_input = parse_aoc_input();
 
     // First star
-    let distance = calculate_distance(&parsed_input);
+    let distance_sum = calculate_distance(&parsed_input);
 
     // Second star
-    let simularity = calculate_simularity(&parsed_input);
+    let simularity_score = calculate_simularity(&parsed_input);
 
     // Record the amount of time it took to run the program
     println!("Time elapsed: {:?}", start.elapsed());
 
-    println!("Calculated distance: {distance}");
-    println!("Similarity value: {simularity}");
+    println!("Calculated distance: {distance_sum}");
+    println!("Similarity value: {simularity_score}");
 }
 
 fn parse_aoc_input() -> InputNumbers {
@@ -34,15 +34,15 @@ fn parse_aoc_input() -> InputNumbers {
             list_two_counted_occurances: HashMap::new(),
         },
         |mut parsed, line| {
-            let left = line[..5].parse::<i32>().unwrap();
-            parsed.list_one_numbers.push(left);
+            let list_one_number = line[..5].parse::<i32>().unwrap();
+            parsed.list_one_numbers.push(list_one_number);
 
-            let right = line[8..].parse::<i32>().unwrap();
-            parsed.list_two_numbers.push(right);
+            let list_two_number = line[8..].parse::<i32>().unwrap();
+            parsed.list_two_numbers.push(list_two_number);
 
             parsed
                 .list_two_counted_occurances
-                .entry(right)
+                .entry(list_two_number)
                 .and_modify(|count| *count += 1)
                 .or_insert(1);
 
@@ -68,18 +68,16 @@ fn calculate_distance(input: &InputNumbers) -> i32 {
 }
 
 fn calculate_simularity(input: &InputNumbers) -> i32 {
-    let score: i32 = input
+    let simularity_score: i32 = input
         .list_one_numbers
         .iter()
-        .map(|left_number| {
-            let encounters_in_right = input
+        .filter_map(|list_one_number| {
+            input
                 .list_two_counted_occurances
-                .get(left_number)
-                .unwrap_or(&0);
-
-            left_number * *encounters_in_right as i32
+                .get(list_one_number)
+                .map(|occurances| list_one_number * *occurances as i32)
         })
         .sum();
 
-    score
+    simularity_score
 }
