8ea536c8e88faadab7245f6190fe9ac043945f59
Sven Brettschneider <svenbrettschneider@proton.me>
2024-12-01 21:05:19 +0100

    using hashmap for faster performance

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,70 +1,88 @@
-use std::fs;
+use std::collections::HashMap;
 
 // Using Winnow parser in the future?
 
 #[derive(Clone)]
 struct InputNumbers {
-    left_numbers: Vec<i32>,
-    right_numbers: Vec<i32>,
+    list_one_numbers: Vec<i32>,
+    list_two_numbers: Vec<i32>,
+    list_two_counted_occurances: HashMap<i32, usize>,
 }
 
 fn main() {
+    let start = std::time::Instant::now();
+
     // print string line by line for file "input.txt"
-    let parsed_input = fs::read_to_string("day_one/input.txt")
-        .unwrap()
-        .lines()
-        .fold(
-            InputNumbers {
-                left_numbers: vec![],
-                right_numbers: vec![],
-            },
-            |mut parsed, line| {
-                let left = line[0..=4].parse::<i32>().unwrap();
-                parsed.left_numbers.push(left);
-
-                let right = line[8..=12].parse::<i32>().unwrap();
-
-                parsed.right_numbers.push(right);
-
-                parsed
-            },
-        );
+    let parsed_input = parse_aoc_input();
 
     // First star
-    calculate_distance(parsed_input.to_owned());
+    let distance = calculate_distance(parsed_input.to_owned());
 
     // Second star
-    calculate_simularity(parsed_input);
+    let simularity = calculate_simularity(parsed_input);
+
+    // Record the amount of time it took to run the program
+    let end = std::time::Instant::now();
+    println!("Time elapsed: {:?}", end - start);
+
+    println!("Calculated distance: {distance}");
+    println!("Similarity value: {simularity}");
 }
 
-fn calculate_distance(mut input: InputNumbers) {
-    input.left_numbers.sort();
-    input.right_numbers.sort();
+fn parse_aoc_input() -> InputNumbers {
+    let mut parsed_input = include_str!("../input.txt").lines().fold(
+        InputNumbers {
+            list_one_numbers: vec![],
+            list_two_numbers: vec![],
+            list_two_counted_occurances: HashMap::new(),
+        },
+        |mut parsed, line| {
+            let left = line[0..=4].parse::<i32>().unwrap();
+            parsed.list_one_numbers.push(left);
 
+            let right = line[8..=12].parse::<i32>().unwrap();
+            parsed.list_two_numbers.push(right);
+
+            parsed
+                .list_two_counted_occurances
+                .entry(right)
+                .and_modify(|count| *count += 1)
+                .or_insert(1);
+
+            parsed
+        },
+    );
+
+    parsed_input.list_one_numbers.sort();
+    parsed_input.list_two_numbers.sort();
+
+    parsed_input
+}
+
+fn calculate_distance(input: InputNumbers) -> i32 {
     let total_difference: i32 = input
-        .left_numbers
+        .list_one_numbers
         .iter()
-        .zip(input.right_numbers)
+        .zip(input.list_two_numbers)
         .map(|(a, b)| i32::abs(a - b))
         .sum();
 
-    println!("Total difference: {}", total_difference);
+    total_difference
 }
 
-fn calculate_simularity(input: InputNumbers) {
+fn calculate_simularity(input: InputNumbers) -> i32 {
     let score: i32 = input
-        .left_numbers
+        .list_one_numbers
         .iter()
         .map(|left_number| {
             let encounters_in_right = input
-                .right_numbers
-                .iter()
-                .filter(|right_number| left_number == *right_number)
-                .count();
+                .list_two_counted_occurances
+                .get(left_number)
+                .unwrap_or(&0);
 
-            left_number * encounters_in_right as i32
+            left_number * *encounters_in_right as i32
         })
         .sum();
 
-    println!("Score: {}", score);
+    score
 }
