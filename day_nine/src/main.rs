1dc85e485e070cd3e21174fc875f0d6882f5e0e2
Sven Brettschneider <svenbrettschneider@proton.me>
2024-12-18 14:51:26 +0100

    refactor day 9

--- day_nine/src/main.rs
+++ day_nine/src/main.rs
@@ -4,64 +4,56 @@ fn main() {
     let input = include_str!("../input.txt");
 
     // Just map all to u8 (except last newline character)
-    let input: Vec<u8> = input[..input.len() - 1]
+    let disk_map: Vec<u8> = input[..input.len() - 1]
         .chars()
         .map(|char| char.to_digit(10).unwrap() as u8)
         .collect();
 
-    part_one(&input)
+    part_one(&disk_map)
 }
 
-fn part_one(input: &[u8]) {
-    let mut files_from_end = input.iter().copied().step_by(2).enumerate().rev();
-    let mut file_from_end = successors(files_from_end.next(), |(file_id, count)| {
-        let new_count = count - 1;
-
-        if new_count > 0 {
-            Some((*file_id, new_count))
-        } else if let Some((next_file_id, next_count)) = files_from_end.next() {
-            Some((next_file_id, next_count))
-        } else {
-            None
-        }
-    });
-
-    let mut files_from_start = input.iter().copied().step_by(2).enumerate();
-    let mut file_from_start = successors(files_from_start.next(), |(file_id, count)| {
-        let new_count = count - 1;
-
-        if new_count > 0 {
-            Some((*file_id, new_count))
-        } else if let Some((next_file_id, next_count)) = files_from_start.next() {
-            Some((next_file_id, next_count))
-        } else {
-            None
-        }
-    });
-
-    let mut disk_index = 0..;
-    let total_file_usage: usize = input.iter().step_by(2).map(|f| *f as usize).sum();
-
-    let a: u64 = input
-        .iter()
-        .enumerate()
-        .flat_map(|(input_index, count)| {
-            if input_index % 2 == 0 {
-                file_from_start
-                    .by_ref()
-                    .take(*count as usize)
-                    .map(|(file_id, _)| disk_index.next().unwrap() as u64 * file_id as u64)
-                    .collect::<Vec<_>>()
+fn part_one(disk_map: &[u8]) {
+    fn create_file_iter(
+        mut file_location_iter: impl Iterator<Item = (usize, u8)>,
+    ) -> impl Iterator<Item = (usize, u8)> {
+        successors(file_location_iter.next(), move |(file_id, count)| {
+            let new_count = count - 1;
+            if new_count > 0 {
+                Some((*file_id, new_count))
+            } else if let Some((next_file_id, next_count)) = file_location_iter.next() {
+                Some((next_file_id, next_count))
             } else {
-                file_from_end
-                    .by_ref()
-                    .take(*count as usize)
-                    .map(|(file_id, _)| disk_index.next().unwrap() as u64 * file_id as u64)
-                    .collect::<Vec<_>>()
+                None
             }
         })
-        .take(total_file_usage)
+    }
+
+    let mut file_iter_from_start =
+        create_file_iter(disk_map.iter().copied().step_by(2).enumerate());
+    let mut file_iter_from_end =
+        create_file_iter(disk_map.iter().copied().step_by(2).enumerate().rev());
+
+    let mut disk_block_index = 0..;
+    let disk_capacity_used_by_files: usize = disk_map.iter().step_by(2).map(|f| *f as usize).sum();
+
+    let filesystem_checksum: u64 = disk_map
+        .iter()
+        .enumerate()
+        .flat_map(|(disk_map_index, count)| {
+            let iter: &mut dyn Iterator<Item = (usize, u8)> = if disk_map_index % 2 == 0 {
+                // if reading file
+                &mut file_iter_from_start
+            } else {
+                // if reading free space
+                &mut file_iter_from_end
+            };
+
+            iter.take(*count as usize)
+                .map(|(file_id, _)| disk_block_index.next().unwrap_or(0) as u64 * file_id as u64)
+                .collect::<Vec<_>>()
+        })
+        .take(disk_capacity_used_by_files)
         .sum();
 
-    println!("The checksum is: {}", a);
+    println!("The checksum is: {}", filesystem_checksum);
 }
