f779d37535abc50a64bf56767dc12ff2c9321857
Sven Brettschneider <svenbrettschneider@proton.me>
2025-01-27 13:30:54 +0100

    aoc twelve start

--- day_twelve/src/main.rs
+++ day_twelve/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Something on top then?");
+    let input = include_str!("../input.txt");
+    println!("{input}");
+    println!("so this is cool as well");
+    println!("I don't see it!");
+    println!("why is this suddenly changing?");
+}
+
+fn this_is_try(legup: i32) {
+    let a = "this is healthy";
+    let b = "this is another thing";
 }
