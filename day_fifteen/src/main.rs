ef7f2bea98665852e60a37f01433c591250483ba
Sven Brettschneider <svenbrettschneider@proton.me>
2025-02-27 17:49:31 +0100

    yeah part 1...

--- day_fifteen/src/main.rs
+++ day_fifteen/src/main.rs
@@ -6,7 +6,8 @@ use winnow::token::one_of;
 use winnow::Parser;
 
 fn main() {
-    let mut input = parse_input();
+    let input = include_str!("../input.txt");
+    let input = parse_input(input);
 
     // Robot starts at middle of map
     let robot_start_position = IVec2 {
@@ -14,16 +15,21 @@ fn main() {
         y: (input.warehouse.height / 2 - 1) as i32,
     };
 
-    println!("{:?}", input.warehouse.contents[1]);
-    input.warehouse.push(IVec2 { x: 24, y: 1 }, &BotMove::Right);
-    println!("{:?}", input.warehouse.contents[1]);
-
-    println!("{:#?}", robot_start_position);
+    part_one(robot_start_position, input.to_owned());
 }
 
-fn parse_input() -> AoCInput {
-    let mut input = include_str!("../input.txt");
+fn part_one(robot_start: IVec2, mut input: AoCInput) {
+    let mut robot_location = robot_start;
 
+    // Go to each direction (specified in AoC input)
+    input.bot_directions.iter().for_each(|direction| {
+        robot_location = input.warehouse.push(robot_location, direction);
+    });
+
+    println!("Part 1: {}", input.warehouse.calc_gps_all_crates());
+}
+
+fn parse_input(mut input: &str) -> AoCInput {
     let warehouse = parse_warehouse(&mut input).unwrap();
     let bot_directions = parse_bot_directions(&mut input).unwrap();
 
@@ -102,46 +108,21 @@ enum BotMove {
 }
 
 impl BotMove {
-    fn get_next_vec(&self, current_location: IVec2) -> IVec2 {
+    fn direction_vector(&self) -> IVec2 {
         match self {
-            BotMove::Up => IVec2 {
-                x: current_location.x,
-                y: current_location.y - 1,
-            },
-            BotMove::Right => IVec2 {
-                x: current_location.x + 1,
-                y: current_location.y,
-            },
-            BotMove::Down => IVec2 {
-                x: current_location.x,
-                y: current_location.y + 1,
-            },
-            BotMove::Left => IVec2 {
-                x: current_location.x - 1,
-                y: current_location.y,
-            },
+            BotMove::Up => IVec2 { x: 0, y: -1 },
+            BotMove::Right => IVec2 { x: 1, y: 0 },
+            BotMove::Down => IVec2 { x: 0, y: 1 },
+            BotMove::Left => IVec2 { x: -1, y: 0 },
         }
     }
 
+    fn get_next_vec(&self, current_location: IVec2) -> IVec2 {
+        current_location + self.direction_vector()
+    }
+
     fn get_prev_vec(&self, current_location: IVec2) -> IVec2 {
-        match self {
-            BotMove::Up => IVec2 {
-                x: current_location.x,
-                y: current_location.y + 1,
-            },
-            BotMove::Right => IVec2 {
-                x: current_location.x - 1,
-                y: current_location.y,
-            },
-            BotMove::Down => IVec2 {
-                x: current_location.x,
-                y: current_location.y - 1,
-            },
-            BotMove::Left => IVec2 {
-                x: current_location.x + 1,
-                y: current_location.y,
-            },
-        }
+        current_location - self.direction_vector()
     }
 }
 
@@ -163,14 +144,25 @@ impl Warehouse {
     fn push(&mut self, pusher_location: IVec2, direction: &BotMove) -> IVec2 {
         let next_location = direction.get_next_vec(pusher_location);
 
+        // Check what content is at the next location
         let new_pusher_location =
             match &self.contents[next_location.y as usize][next_location.x as usize] {
+                // If there's a box, we need to move it to the location closest to the wall that is empty
                 Content::Box => {
-                    let new_pusher_location = self.push(next_location, direction);
-                    self.move_item(next_location, new_pusher_location);
-                    direction.get_prev_vec(new_pusher_location)
+                    // Recursively push the box
+                    let box_destination = self.push(next_location, direction);
+
+                    // Move the box to its new location
+                    self.move_item(next_location, box_destination);
+
+                    // Return the position where the pusher should end up (behind the box)
+                    direction.get_prev_vec(box_destination)
                 }
-                Content::Empty => self.push(next_location, direction),
+
+                // If the next space is empty, continue pushing in that direction
+                Content::Empty => next_location,
+
+                // If there's a wall, we can't move, so stay at the current location
                 Content::Wall => pusher_location,
             };
 
@@ -183,5 +175,35 @@ impl Warehouse {
             Content::Empty,
         );
         self.contents[target_location.y as usize][target_location.x as usize] = source_value;
+    }
+
+    fn calc_gps_all_crates(&self) -> usize {
+        (0..self.height)
+            .flat_map(|row_index| {
+                (0..self.width).filter_map(move |column_index| {
+                    if matches!(self.contents[row_index][column_index], Content::Box) {
+                        Some(row_index * 100 + column_index)
+                    } else {
+                        None
+                    }
+                })
+            })
+            .sum()
+    }
+
+    fn print(&self) {
+        for row in &self.contents {
+            for content in row {
+                print!(
+                    "{}",
+                    match content {
+                        Content::Box => 'O',
+                        Content::Empty => '.',
+                        Content::Wall => '#',
+                    }
+                );
+            }
+            println!();
+        }
     }
 }
